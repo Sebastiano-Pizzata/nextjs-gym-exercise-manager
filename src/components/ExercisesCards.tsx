50563f977418a825e94fe3c0337e9a0695aa3f48
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-12-02 13:27:29 +0100

    ADD: homepage cards

--- src/components/ExercisesCards.tsx
+++ src/components/ExercisesCards.tsx
@@ -10,12 +10,32 @@ export default function ExercisesCards() {
     }, []);
 
     return (
-        <div>
-            <ul>
-                {exercises.map((e) => (
-                    <li key={e.id}>{e.name}</li>
-                ))}
-            </ul>
+        <div className="container mx-auto flex flex-wrap justify-center gap-6">
+            {exercises.map((e) => (
+                <div
+                    key={e.name}
+                    className="w-72 h-70 bg-white rounded-lg shadow-md overflow-hidden flex flex-col
+                    transform transition-transform duration-300 hover:scale-105"
+                >
+                    <img
+                        src={e.image}
+                        alt={e.name}
+                        className="w-full h-40 object-cover"
+                    />
+
+                    <div className="p-4 flex flex-col flex-grow">
+                        <h3 className="text-xl font-semibold mb-2">{e.name}</h3>
+
+                        <p className="text-gray-600 line-clamp-4">
+                            {e.description}
+                        </p>
+
+                        <div className="mt-auto"></div>
+                    </div>
+                </div>
+            ))}
         </div>
+
+
     )
 }
